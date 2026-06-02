0a2cd27569458d6831933772e7c14a2b2f617f80
Victor Santos <victorsantospe1@gmail.com>
2026-06-02 13:31:30 -0300

    .

--- frontend/hdpaMaster/src/pages/Faculdades/Usp/Usp.jsx
+++ frontend/hdpaMaster/src/pages/Faculdades/Usp/Usp.jsx
@@ -1,45 +1,13 @@
-import logoImage from "../../assets/LOGO2.png";
 import React from "react";
-import styles from "./Header.module.css";
+import styles from "./Usp.module.css";
+import Header from "../../../components/Header/Header";
 
-import {
-  BookOpen,
-  ClipboardPen,
-  BarChart3,
-  UserCircle2,
-  ChevronLeft,
-  ChevronRight,
-} from "lucide-react";
-
-function Header() {
+function Usp() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logoArea}>
-        <img src={logoImage} alt="Logo" className={styles.logoImage} />
-      </div>
-
-      <div className={styles.menu}>
-        <div className={styles.menuItem}>
-          <BookOpen size={24} />
-          <span>Banco de Questões</span>
-        </div>
-
-        <div className={styles.menuItem}>
-          <ClipboardPen size={24} />
-          <span>Simulados</span>
-        </div>
-
-        <div className={styles.menuItem}>
-          <BarChart3 size={24} />
-          <span>Meu Desempenho</span>
-        </div>
-      </div>
-
-      <button className={styles.profileButton}>
-        <UserCircle2 size={34} />
-      </button>
-    </header>
+    <div className={styles.container}>
+      <Header />
+    </div>
   );
 }
 
-export default Header;
+export default Usp;
