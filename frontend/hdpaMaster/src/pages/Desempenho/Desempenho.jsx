84aee5ae772cae98e0a5fcfbae011ce824c04d3c
ThiagoAlmeida2407 <lucianapaula97192@gmail.com>
2026-06-09 15:53:29 -0300

    .

--- frontend/hdpaMaster/src/pages/Desempenho/Desempenho.jsx
+++ frontend/hdpaMaster/src/pages/Desempenho/Desempenho.jsx
@@ -1,10 +1,10 @@
 import styles from "./Desempenho.module.css";
-import Header from "../../../components/Header/Header";
+import Header from "../../components/Header/Header";
 export default function Desempenho() {
   return (
     <div className={styles.container}>
       <Header />
-      
+
       <div className={styles.topBar}></div>
 
       <div className={styles.content}>
